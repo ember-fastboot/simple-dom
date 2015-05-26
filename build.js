ca849069e11f8a2761a0ea15bd0e6995662c3249
Justin Meyer <justinbmeyer@gmail.com>
2015-05-26 15:56:09 -0500

    adding build

---
+++ build.js
@@ -0,0 +1,25 @@
+var stealTools = require("steal-tools");
+
+stealTools.export({
+	system: {
+		config: __dirname+"/package.json!npm"
+	},
+	outputs: {
+		"+amd": {},
+		"+global-js": {},
+		"+cjs": {}
+	}
+}).catch(function(e){
+	
+	setTimeout(function(){
+		throw e;
+	},1);
+	
+});
+
+
+
+
+
+
+
