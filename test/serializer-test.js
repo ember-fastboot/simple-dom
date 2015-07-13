ed95c5ba7aadcf24e4fc0a1df8cf3f464a307cc0
Garth Poitras <garth22@gmail.com>
2015-07-13 16:38:45 -0400

    [BUGFIX] Do not serialize a node's siblings

--- test/serializer-test.js
+++ test/serializer-test.js
@@ -10,15 +10,45 @@ QUnit.module('Serializer', {
   }
 });
 
-QUnit.test('serializes correctly', function (assert) {
+QUnit.test('serializes single element correctly', function (assert) {
+  var actual = this.serializer.serialize(element('div'));
+  assert.equal(actual, '<div></div>');
+});
+
+QUnit.test('serializes single void element correctly', function (assert) {
+  var actual = this.serializer.serialize(element('img', { src: 'foo' }));
+  assert.equal(actual, '<img src="foo">');
+});
+
+QUnit.test('serializes complex tree correctly', function (assert) {
   var actual = this.serializer.serialize(fragment(
-    element('div', { id:'foo' },
+    element('div', { id: 'foo' },
       element('b', {},
         text('Foo & Bar')
-      )
+      ),
+      text('!'),
+      element('img', { src: 'foo' })
     )
   ));
-  assert.equal(actual, '<div id="foo"><b>Foo &amp; Bar</b></div>');
+  assert.equal(actual, '<div id="foo"><b>Foo &amp; Bar</b>!<img src="foo"></div>');
+});
+
+QUnit.test('does not serialize siblings of an element', function (assert) {
+  var html = element('html');
+  var head = element('head');
+  var body = element('body');
+
+  head.appendChild(element('meta', { content: 'foo' }));
+  head.appendChild(element('meta', { content: 'bar' }));
+
+  html.appendChild(head);
+  html.appendChild(body);
+
+  var actual = this.serializer.serialize(head);
+  assert.equal(actual, '<head><meta content="foo"><meta content="bar"></head>');
+
+  actual = this.serializer.serialize(body);
+  assert.equal(actual, '<body></body>');
 });
 
 // SimpleDOM supports an extension of the DOM API that allows inserting strings of
