7dc427bd4bfc071519c29c94cc21d8cdefbb3fc3
Kris Selden <kris.selden@gmail.com>
2017-09-07 16:12:43 -0700

    fix sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,13 @@
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import buble from 'rollup-plugin-buble';
+
 export default {
   input: 'lib/simple-dom.js',
   name: 'SimpleDOM',
+  plugins: [
+    sourcemaps(),
+    buble(),
+  ],
   output: {
     file: 'dist/simple-dom.js',
     format: 'umd',
