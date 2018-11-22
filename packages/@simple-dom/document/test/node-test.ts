8818b831f593b6e486075171289beacb29b70072
Tom Dale <tom@tomdale.net>
2018-11-21 21:17:12 -0500

    nodeValue property on comment and text nodes should be mutable

--- packages/@simple-dom/document/test/node-test.ts
+++ packages/@simple-dom/document/test/node-test.ts
@@ -29,4 +29,18 @@ moduleWithDocument('Node', (helper) => {
     assert.strictEqual(body.lastChild, div, 'parents last child is set');
   });
 
+  QUnit.test('nodeValue is mutable', (assert) => {
+    const doc = helper.document;
+    const text = doc.createTextNode('hello world');
+    const comment = doc.createComment('goodbye cruel world');
+
+    assert.strictEqual(text.nodeValue, 'hello world', 'precond - node value is set');
+    assert.strictEqual(comment.nodeValue, 'goodbye cruel world', 'precond - node value is set');
+
+    text.nodeValue = text.nodeValue.toUpperCase();
+    comment.nodeValue = comment.nodeValue.toUpperCase();
+
+    assert.strictEqual(text.nodeValue, 'HELLO WORLD');
+    assert.strictEqual(comment.nodeValue, 'GOODBYE CRUEL WORLD', 'precond - node value is set');
+  });
 });
